b56ca50a26240897d74e6564d27647df5cd4aea6
deadlycrawler <deadlycrawler2@gmail.com>
2018-10-17 15:18:54 -0600

    excaption handling has been implimented

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,11 @@
 package sample;
 
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 
@@ -17,9 +15,11 @@ public class Main extends Application {
         primaryStage.setTitle("hex goes on top, binary on the bottom");
 
         TextArea textArea = new TextArea();
+        textArea.setPromptText("Enter up to 8 digit hex codes here, separated by a space");
+
 
         TextArea textArea2 = new TextArea();
-
+        textArea2.setPromptText("Enter up to 31 Bit binary values here, separated by a space");
 
         Button DisplayBinaryButton = new Button("Click to Convert to binary");
         DisplayBinaryButton.setMinWidth(50);
@@ -37,7 +37,7 @@ public class Main extends Application {
 
         });
 
-        VBox vbox = new VBox(textArea, DisplayBinaryButton, DisplayHexButton, textArea2);
+        VBox vbox = new VBox(DisplayBinaryButton, textArea, DisplayHexButton, textArea2);
 
         Scene scene = new Scene(vbox, 1300, 1000 / 16 * 9);
         primaryStage.setScene(scene);
@@ -61,17 +61,28 @@ public class Main extends Application {
     }
 
     String convertToHex(String BinaryString) {
-        BinaryString = BinaryString.replace("\n"," ");
+        BinaryString = BinaryString.replace("\n", " ");
         BinaryString = BinaryString.trim();
-        BinaryString = BinaryString.replace("  "," ");
+        BinaryString = BinaryString.replace("  ", " ");
         String[] SeparatedBinary = BinaryString.split(" ");
 
 
         String Hex = "";
 
         for (int i = 0; i < SeparatedBinary.length; i++) {
-            int decimal = Integer.parseInt(SeparatedBinary[i], 2);
-            Hex = Hex + Integer.toString(decimal, 16) + " ";
+
+            try {
+                int decimal = Integer.parseInt(SeparatedBinary[i], 2);
+                Hex = Hex + Integer.toString(decimal, 16) + " ";
+            } catch (NumberFormatException e) {
+                Hex = "Check formatting";
+                break;
+            }
+
+            if (i % 40 == 0 && i != 0) {
+
+                Hex += "\n";
+            }
         }
 
         Hex = Hex.trim();
@@ -80,16 +91,21 @@ public class Main extends Application {
     }
 
     String convertToBin(String HexString) {
-        HexString = HexString.replace("\n"," ");
+        HexString = HexString.replace("\n", " ");
         HexString = HexString.trim();
-        HexString = HexString.replace("  "," ");
+        HexString = HexString.replace("  ", " ");
         String[] SeperatedHex = HexString.split(" ");
         String Bin = "";
 
         for (int i = 0; i < SeperatedHex.length; i++) {
 
 
-            Bin = Bin + Integer.toBinaryString(Integer.parseInt(SeperatedHex[i], 16)) + " ";
+            try {
+                Bin = Bin + Integer.toBinaryString(Integer.parseInt(SeperatedHex[i], 16)) + " ";
+            } catch (NumberFormatException e) {
+                Bin = "Check formatting";
+                break;
+            }
 
             if (i % 10 == 0 && i != 0) {
 
